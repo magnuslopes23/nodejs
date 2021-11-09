e029f4d8ea106bbfe0f2cf6d15dc9000cf10d6c2
magnuslopes23 <lopesmagnus2324@gmail.com>
2021-11-10 00:40:03 +0530

    routing and redirecting requests

--- getting-started-with-basics/app.js
+++ getting-started-with-basics/app.js
@@ -1,10 +1,32 @@
 // import or require the core http moudle form js/node
 const http = require('http');
+const fs = require('fs');
 
 // create a server using the http module which has access to createServer method.
 //this returns a function and we store it in a variabe called server
 const server = http.createServer((req, res)=>{
-    console.log(res);
+
+    console.log(req.url,req.headers);
+    const url= req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method ==="POST"){
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode =302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body><h1>My first app</h1></body>')
+    res.write('</html>');
+    res.end();
 });
 
 // here we use the server method and listen to incoming requests.
