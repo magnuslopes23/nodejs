759407dcd13a4285daa5dee19456f61d6c3da4a6
magnuslopes23 <lopesmagnus2324@gmail.com>
2021-11-10 01:25:56 +0530

    using writeFile insted of writeFileSync

--- getting-started-with-basics/app.js
+++ getting-started-with-basics/app.js
@@ -21,14 +21,15 @@ const server = http.createServer((req, res)=>{
         req.on('data', (chunk) =>{
             body.push(chunk);
         })
-        req.on('end', ()=>{
+        return req.on('end', ()=>{
             const bodyParsed = Buffer.concat(body).toString();
             const message = bodyParsed.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err) =>{
+                res.statusCode =302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         })
-        res.statusCode =302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
