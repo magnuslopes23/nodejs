2557a1c16d5ec9364dab3517b69588c902ebbc50
magnuslopes23 <lopesmagnus2324@gmail.com>
2021-11-13 12:18:48 +0530

    adding routes

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 const app = express();
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use("/add-products", (req, res, next)=>{
-    res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
-    
+app.use(adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next)=>{
+    res.status(404).send("<h1>Page not found</h1>");
 });
 
-app.post("/products", (req, res, next)=>{
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use("/", (req, res, next)=>{
-    res.send('<h1>wecIcome</h1>');
-});
-
-app.listen(3000)
+app.listen(3000);
---
+++ express-basics/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-products", (req, res, next)=>{
+    res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+    
+});
+
+router.post("/products", (req, res, next)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+module.exports = router;
