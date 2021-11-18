4541591d0118e3275ca51d2389b7203ac368781a
magnuslopes23 <lopesmagnus2324@gmail.com>
2021-11-18 23:45:09 +0530

    working with pug

--- express-basics/routes/admin.js
+++ express-basics/routes/admin.js
@@ -6,18 +6,18 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const product = [];
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  product.push( {title: req.body.title,});
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-exports.router = router;
-exports.product = product;
+exports.routes = router;
+exports.products = products;
